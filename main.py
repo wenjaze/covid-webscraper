000e56483e85083ee80f3506be13aceef24f2119
rg <raska.gergely@gmail.com>
2021-05-28 08:16:08 +0200

    Code smell upgrade

--- main.py
+++ main.py
@@ -33,30 +33,30 @@ def dataFrameToNumpyArray():
 def fillCountriesList():
     df = dataFrameToNumpyArray()
     countries = []
-    for row in df:
-        c = country.Country(
-            row[1],
-            row[2],
-            row[3],
-            row[4],
-            row[5],
-            row[6],
-            row[7],
-            row[8],
-            row[9],
-            row[10],
-            row[11],
-            row[12],
-            row[13],
-            row[14],
-            row[15],
-            row[16],
-            row[17],
-            row[18],
-            row[19],
-            row[20],
-            row[21])
-        countries.append(c)
+    for row in df[7:]:
+        countries.append(
+            country.Country(
+                row[1],
+                row[2],
+                row[3],
+                row[4],
+                row[5],
+                row[6],
+                row[7],
+                row[8],
+                row[9],
+                row[10],
+                row[11],
+                row[12],
+                row[13],
+                row[14],
+                row[15],
+                row[16],
+                row[17],
+                row[18],
+                row[19],
+                row[20],
+                row[21]))
     return countries
 
 
