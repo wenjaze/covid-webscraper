54201563aa9a28d87aab176b94f57c85bf790c52
rg <raska.gergely@gmail.com>
2021-05-28 08:12:47 +0200

    Refactor code

--- country.py
+++ country.py
@@ -1,46 +1,53 @@
 class Country(object):
-	def __init__(
-		self,
-		name,
-		totalCases,
-		newCases,
-		totalDeaths,
-		newDeaths,
-		totalRecovered,
-		newRecovered,
-		activeCases,
-		criticalCases,
-		casesPerOneMillion,
-		deathsPerOneMillion,
-		totalTests,
-		testsPerOneMillion,
-		population,
-		oneCaseEveryXPeople,
-		oneDeathEveryXPeople,
-		oneTestEveryXPeople,
-		newCasesPerOneMillion,
-		newDeathPerOneMillion,
-		activeCasesPerOneMillion,
-		):
-		self.name = name
-		self.totalCases = totalCases
-		self.newCases = newCases
-		self.totalDeaths = totalDeaths
-		self.newDeaths = newDeaths
-		self.totalRecovered = totalRecovered
-		self.newRecovered = newRecovered
-		self.activeCases = activeCases
-		self.criticalCases = criticalCases
-		self.casesPerOneMillion = casesPerOneMillion
-		self.deathsPerOneMillion = deathsPerOneMillion
-		self.totalTests = totalTests
-		self.testsPerOneMillion = testsPerOneMillion
-		self.population = population
-		self.oneCaseEveryXPeople = oneCaseEveryXPeople
-		self.oneDeathEveryXPeople = oneDeathEveryXPeople
-		self.oneTestEveryXPeople = oneTestEveryXPeople
-		self.newCasesPerOneMillion = newCasesPerOneMillion
-		self.newDeathPerOneMillion = newDeathPerOneMillion
-		self.activeCasesPerOneMillion = activeCasesPerOneMillion
+    def __init__(
+            self,
+            name,
+            totalCases,
+            newCases,
+            totalDeaths,
+            newDeaths,
+            totalRecovered,
+            newRecovered,
+            activeCases,
+            criticalCases,
+            casesPerOneMillion,
+            deathsPerOneMillion,
+            totalTests,
+            testsPerOneMillion,
+            population,
+            continent,
+            oneCaseEveryXPeople,
+            oneDeathEveryXPeople,
+            oneTestEveryXPeople,
+            newCasesPerOneMillion,
+            newDeathPerOneMillion,
+            activeCasesPerOneMillion,
+    ):
+        self.name = name
+        self.totalCases = totalCases
+        self.newCases = newCases
+        self.totalDeaths = totalDeaths
+        self.newDeaths = newDeaths
+        self.totalRecovered = totalRecovered
+        self.newRecovered = newRecovered
+        self.activeCases = activeCases
+        self.criticalCases = criticalCases
+        self.casesPerOneMillion = casesPerOneMillion
+        self.deathsPerOneMillion = deathsPerOneMillion
+        self.totalTests = totalTests
+        self.testsPerOneMillion = testsPerOneMillion
+        self.population = population
+        self.continent = continent
+        self.oneCaseEveryXPeople = oneCaseEveryXPeople
+        self.oneDeathEveryXPeople = oneDeathEveryXPeople
+        self.oneTestEveryXPeople = oneTestEveryXPeople
+        self.newCasesPerOneMillion = newCasesPerOneMillion
+        self.newDeathPerOneMillion = newDeathPerOneMillion
+        self.activeCasesPerOneMillion = activeCasesPerOneMillion
 
-	# TODO : __repr__ def __str__(self):
+    # TODO : __repr__ def __str__(self):
+    def __str__(self):
+        return "Name: " + self.name + "\nTotal cases: " + self.totalCases + "\nTotal deaths: " + self.totalDeaths
+
+    def __repr__(self):
+        return "<Country.object name = " + self.name + ">"
--- main.py
+++ main.py
@@ -4,64 +4,66 @@ import pandas as pd
 import country
 
 
-URL = 'https://www.worldometers.info/coronavirus/'
-page = requests.get(URL)
-
-soup = BeautifulSoup(page.content, 'html.parser')
-
-table_data = soup.find(id='main_table_countries_today')
-
-
-# for c in countries:
-# 	if c.find('a') != None:
-# 		a.append(c.find('a').text.strip())
+def getTable():
+    URL = "https://www.worldometers.info/coronavirus/"
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    return soup.find(id='main_table_countries_today')
 
 
-# print(a)
-# for s in soup:
-# 	print(soup)
-headers = []
-for i in table_data.find_all('th'):
-    title = i.text
-    headers.append(title)
+def createDataFrame():
+    headers = []
+    for i in getTable().find_all('th'):
+        title = i.text
+        headers.append(title)
 
-df = pd.DataFrame(columns = headers)
+    return pd.DataFrame(columns=headers)
 
-for j in table_data.find_all('tr')[1:]:
+
+def dataFrameToNumpyArray():
+    df = createDataFrame()
+    for j in getTable().find_all('tr')[1:]:
         row_data = j.find_all('td')
         row = [tr.text for tr in row_data]
         length = len(df)
-        df.loc[length] = row  
-df = df.to_numpy()
+        df.loc[length] = row
+    return df.to_numpy()
 
-countries = []
 
-for row in df:
-	c = country.Country(
-		row[1],
-		row[2],
-		row[3],
-		row[4],
-		row[5],
-		row[6],
-		row[7],
-		row[8],
-		row[9],
-		row[10],
-		row[11],
-		row[12],
-		row[13],
-		row[14],
-		row[15],
-		row[16],
-		row[17],
-		row[18],
-		row[19],
-		row[20])
-	countries.append(c)
+def fillCountriesList():
+    df = dataFrameToNumpyArray()
+    countries = []
+    for row in df:
+        c = country.Country(
+            row[1],
+            row[2],
+            row[3],
+            row[4],
+            row[5],
+            row[6],
+            row[7],
+            row[8],
+            row[9],
+            row[10],
+            row[11],
+            row[12],
+            row[13],
+            row[14],
+            row[15],
+            row[16],
+            row[17],
+            row[18],
+            row[19],
+            row[20],
+            row[21])
+        countries.append(c)
+    return countries
 
-country_name = input("Enter a country:")
 
-for k in countries:
-	if k.name == country_name:
-		print(str(k))
+def printAllCountries(countries):
+    for k in countries:
+        print(vars(k))
+
+
+if __name__ == "__main__":
+    printAllCountries(fillCountriesList())
